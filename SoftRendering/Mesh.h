a98167af0257679ab1b61df25c4d2ebd28f71291
xiqv <xiqv926@163.com>
2016-07-17 21:35:43 +0800

    Color Vertex

---
+++ SoftRendering/Mesh.h
@@ -0,0 +1,13 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector>
+
+#include "Vertex.h"
+
+class Mesh {
+public:
+	
+};
+
+#endif
--- SoftRendering/main.cpp
+++ SoftRendering/main.cpp
@@ -2,6 +2,8 @@
 #include "Matrix4x4.h"
 #include "Vector3.h"
 #include "Xmath.h"
+#include "Color.h"
+#include "Vertex.h"
 
 int main() {
 	return 0;
